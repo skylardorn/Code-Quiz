c30a756d0142ee3f1a79d0ae2528183c539f882b
Skylar Dorn <skylarwdorn@gmail.com>
2022-09-19 13:55:03 -0700

    psuedo code

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,13 @@
+let  $timeEl = document.querySelector(".time");
+let  $mainEl = document.getElementById("main");
+let secondsLeft = 30;
+
+// when i go to the website im prompted with a popup "Do you think you have what it takes to defeat me?" says the code quiz
+
+
+// they can either click yes, challenge me. or no I just cant right now. so two button centered buttons waitin to be clicked
+// up in the nav header bar we have the view highscores page an we have the timer. which will move to the center of the screen once the game has been started.
+// when highscores is clicked than 
+// 10 q's 
+
+// gonna have the submit button as well as the 
